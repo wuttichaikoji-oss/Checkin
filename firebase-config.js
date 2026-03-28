543cf493e4c3ef24729b3c1b64bc5bb61c521e44
wuttichaikoji-oss <wuttichaikoji@gmail.com>
2026-03-28 23:02:34 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-// Copy this file, fill your real Firebase config, and keep the filename as firebase-config.js
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyBV64yW94kMyrjw_BRvPyyFlCdK8KEsJQY",
   authDomain: "moringcard.firebaseapp.com",
   projectId: "moringcard",
   storageBucket: "moringcard.firebasestorage.app",
   messagingSenderId: "281984975461",
   appId: "1:281984975461:web:7302964312085875553c8c",
-  measurementId: "G-8V49S3LXYJID"
+  measurementId: "G-8V49S3LXYJ"
 };
 
 window.APP_DEFAULT_OPERATOR = {
